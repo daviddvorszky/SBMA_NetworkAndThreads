d2c6ecf73ea988c27ebbab0bef38ace40e4a8629
xR3l4Xx <halloka.mangoka@gmail.com>
2022-08-30 14:45:00 +0300

    Show text form URL

--- app/src/main/java/com/example/sbma_networkandthreads/MainActivity.kt
+++ app/src/main/java/com/example/sbma_networkandthreads/MainActivity.kt
@@ -8,36 +8,38 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sbma_networkandthreads.ui.theme.SBMA_NetworkAndThreadsTheme
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SBMA_NetworkAndThreadsTheme {
-                // A surface container using the 'background' color from the theme
+                var text by remember { mutableStateOf("Loading")}
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    thread(){
+                        val myUrl = URL("https://users.metropolia.fi/~jarkkov/koe.txt")
+                        val myConn = myUrl.openConnection()
+                        val istream = myConn.getInputStream()
+                        val allText = istream.bufferedReader().use{it.readText()}
+                        val result = StringBuilder()
+                        result.append(allText)
+                        text = result.toString()
+                    }
+                    Text(text = text)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SBMA_NetworkAndThreadsTheme {
-        Greeting("Android")
     }
 }
